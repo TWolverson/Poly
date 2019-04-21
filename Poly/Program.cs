6ac1a65cf5a308a492b1a630f733de23c0e960bb
Tom Wolverson <t_wolverson@hotmail.com>
2019-04-21 13:51:27 +0100

    Update to comprehend chain integrity and be able to make snake moves to set up the initial state

--- Poly/Program.cs
+++ Poly/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Poly
@@ -53,8 +54,26 @@ namespace Poly
         B = 1
     }
 
+
+
     class Program
     {
+        struct Vector
+        {
+            public Vector(int x, int y, int z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+            internal int x; internal int y; internal int z;
+
+            public static Vector operator +(Vector v, LatticeVector delta)
+            {
+                return new Vector(v.x + XUnit(delta), v.y + YUnit(delta), v.z + ZUnit(delta));
+            }
+        }
+
         static int XUnit(LatticeVector vector)
         {
             return (((int)vector >> 0) & 0b11) - 1;
@@ -70,45 +89,206 @@ namespace Poly
             return (((int)vector >> 4) & 0b11) - 1;
         }
 
+        static decimal fillFactor = 0.5M;
+        static int chainSize = 12;
+        static int cellSize = 100;
+        static VacancyType[,,] lattice;
+
+        static LatticeVector[] latticeVectors = (LatticeVector[])Enum.GetValues(typeof(LatticeVector));
+
         static void Main(string[] args)
         {
-            var latticeVectors = (LatticeVector[])Enum.GetValues(typeof(LatticeVector));
 
-            //foreach (var vacancy in latticeVectors)
-            //{
-            //    var x = XUnit(vacancy);
-            //    var y = YUnit(vacancy);
-            //    var z = ZUnit(vacancy);
-            //    Console.WriteLine($"({x},{y},{z})");
-            //}
 
             var randomVacancySelector = new Random();
 
-            var lattice = new VacancyType[100, 100, 100];
-            var xIndex = 0;
-            var yIndex = 0;
-            var zIndex = 0;
-            while (zIndex < 100)
+            var totalVacancies = cellSize * cellSize * cellSize / 2; // only half the number of cubic cells are occupiable
+            var numChains = (int)(totalVacancies * fillFactor / chainSize);
+            numChains = numChains - numChains % 2; // must be even as we want equal distribution of As and Bs
+            Console.WriteLine($"Fill factor: {fillFactor} Computed fill factor: {(double)numChains * chainSize / totalVacancies} Chain size: {chainSize} Computed number of chains: {numChains}");
+
+            lattice = new VacancyType[100, 100, 100];
+            short xIndex = 0;
+            short yIndex = 0;
+            short zIndex = 0;
+
+            var chainsPerRun = cellSize / chainSize;
+            var chainsPerPlane = chainsPerRun * cellSize / 2;
+
+            Console.WriteLine($"Chains per run: {chainsPerRun}");
+            Console.WriteLine($"Chains per plane: {chainsPerPlane}");
+            // Console.Read();
+            Vector[] occupancies = PopulateOccupancies(randomVacancySelector, chainSize, numChains);
+
+            var currentChain = 0;
+            var currentLink = 0;
+
+            while (zIndex < cellSize)
             {
-                while (yIndex < 100)
+                while (yIndex < cellSize)
                 {
-                    while (xIndex < 100)
+                    //Console.Write($"{yIndex})");
+                    while (xIndex < cellSize)
                     {
                         if ((xIndex + yIndex + zIndex) % 2 == 0)
                         {
-                            var fillVacancy = (VacancyType)randomVacancySelector.Next(-1, 2);
-                            // if ((xIndex + yIndex + zIndex) % 500 == 0) { Console.WriteLine($"Setting {xIndex},{yIndex},{zIndex} to type {fillVacancy}"); }
-                            lattice[xIndex, yIndex, zIndex] = fillVacancy;
+
+
+                            currentChain =
+                               xIndex / chainSize  // x
+                               + chainsPerRun * (yIndex / 2)
+                               + zIndex * chainsPerPlane; // z 
+
+                            currentLink = xIndex % chainSize;
+
+                            if (currentChain < numChains && (xIndex - (chainsPerRun * chainSize)) < 0)
+                            {
+                                occupancies[currentChain * chainSize + currentLink] = new Vector(xIndex, yIndex, zIndex);
+                                var fillVacancy = (VacancyType)(1 - ((currentChain % 2) == 0 ? 2 : 0));
+                                lattice[xIndex, yIndex, zIndex] = fillVacancy;
+                            }
+
+                        }
+                        else
+                        {
                         }
                         xIndex++;
+
                     }
                     xIndex = 0;
+
                     yIndex++;
+
                 }
                 yIndex = 0;
                 zIndex++;
             }
             zIndex = 0;
+
+            var lookup = new HashSet<Vector>(occupancies);
+
+            var totalEntropy = CountEntropy();
+
+            bool stop = false;
+            var kT = 5000;
+            var acceptanceFactors = new decimal[12];
+            var numberOfAdjustments = 0;
+            Vector[] updatedPositions = new Vector[chainSize];
+            while (!stop)
+            {
+                // precompute boltzmann factors
+                //for (int i = 1; i < 12; i++)
+                //{
+                //    acceptanceFactors[i - 1] = (decimal)Math.Exp((double)-i / kT);
+                //}
+
+                // perform annealing
+                currentChain = randomVacancySelector.Next(0, numChains);
+
+                // fudge this so that we always choose the head, for now
+                var chosenVacancy = currentChain * chainSize; //randomVacancySelector.Next(0, numChains * chainSize);
+                var chosenVacancyLocation = occupancies[chosenVacancy];
+
+                var randomDirection = (LatticeVector)randomVacancySelector.Next(12);
+                var randomlySelectedNeighbour = chosenVacancyLocation + randomDirection;
+                randomlySelectedNeighbour.x = (randomlySelectedNeighbour.x + cellSize) % cellSize;
+                randomlySelectedNeighbour.y = (randomlySelectedNeighbour.y + cellSize) % cellSize;
+                randomlySelectedNeighbour.z = (randomlySelectedNeighbour.z + cellSize) % cellSize;
+
+                //currentChain = chosenVacancy / chainSize;
+
+                if (lookup.Contains(randomlySelectedNeighbour))
+                {
+                    // the neighbour is occupied; compute a swap
+                }
+                else
+                {
+                    //RenderLayerToConsole();
+                    // the neighbour is unoccupied; determine whether we can move into this vacancy
+
+                    // 1) if we are the head or the tail we can always move into this vacancy
+                    if (chosenVacancy % chainSize == 0)
+                    {
+
+                        Array.Copy(occupancies, chosenVacancy, updatedPositions, 1, chainSize - 1);
+                        updatedPositions[0] = randomlySelectedNeighbour;
+                        lookup.Add(randomlySelectedNeighbour);
+
+                        // does not work if the vacancy chosen happens to be the very last one
+                        Vector vacatedPosition = occupancies[chosenVacancy];
+
+                        // compare the neighbour interactions between the two configurations
+                        int currentBindings = 0;
+
+                        for (int linkIndex = 0; linkIndex < chainSize; linkIndex++)
+                        {
+                            Vector linkCoords = occupancies[chosenVacancy + linkIndex];
+                            VacancyType linkType = lattice[linkCoords.x, linkCoords.y, linkCoords.z];
+                            for (int neighbourIndex = 0; neighbourIndex < 12; neighbourIndex++)
+                            {
+                                LatticeVector neighbourOrientation = (LatticeVector)neighbourIndex;
+                                VacancyType neighbourType = lattice[
+                                    (linkCoords.x + XUnit(neighbourOrientation) + cellSize) % cellSize,
+                                    (linkCoords.y + YUnit(neighbourOrientation) + cellSize) % cellSize,
+                                    (linkCoords.z + ZUnit(neighbourOrientation) + cellSize) % cellSize];
+
+                                currentBindings += (int)neighbourType * (int)linkType;
+                            }
+                        }
+
+                        int newBindings = 0;
+
+                        for (int linkIndex = 0; linkIndex < chainSize; linkIndex++)
+                        {
+                            Vector linkCoords = updatedPositions[linkIndex];
+                            VacancyType linkType = lattice[linkCoords.x, linkCoords.y, linkCoords.z];
+                            for (int neighbourIndex = 0; neighbourIndex < 12; neighbourIndex++)
+                            {
+                                LatticeVector neighbourOrientation = (LatticeVector)neighbourIndex;
+                                VacancyType neighbourType = lattice[
+                                    (linkCoords.x + XUnit(neighbourOrientation) + cellSize) % cellSize,
+                                    (linkCoords.y + YUnit(neighbourOrientation) + cellSize) % cellSize,
+                                    (linkCoords.z + ZUnit(neighbourOrientation) + cellSize) % cellSize];
+
+                                newBindings += (int)neighbourType * (int)linkType;
+                            }
+                        }
+
+                        Array.Copy(updatedPositions, 0, occupancies, chosenVacancy, chainSize);
+
+
+                        var thisChainVacancyType = (VacancyType)(1 - ((currentChain % 2) == 0 ? 2 : 0));
+
+                        numberOfAdjustments++;
+                        foreach (var vector in updatedPositions)
+                        {
+                            var chainIndex = chosenVacancy / numChains;
+                            lattice[vector.x, vector.y, vector.z] = thisChainVacancyType;
+                        }
+
+                        lattice[vacatedPosition.x, vacatedPosition.y, vacatedPosition.z] = VacancyType.Empty;
+                        lookup.Remove(vacatedPosition);
+
+                        if (numberOfAdjustments % 1000 == 0) RenderLayerToConsole();
+
+                    }
+                }
+
+
+                //kT /= 2;
+                //stop = kT <= 1;
+            }
+
+            Console.WriteLine($"Total entropy {totalEntropy}");
+
+            Console.ReadLine();
+        }
+
+        private static int CountEntropy()
+        {
+            short xIndex = 0;
+            short yIndex = 0;
+            short zIndex = 0;
 
             var totalEntropy = 0;
 
@@ -122,9 +302,16 @@ namespace Poly
                         {
                             var thisVacancy = lattice[xIndex, yIndex, zIndex];
                             if (thisVacancy == (VacancyType)0) { xIndex++; continue; }
+
+                            // count the number of interaction contributions with this site's neighbours
+                            // double-counting the same pair of sites is intentional
                             foreach (var vector in latticeVectors)
                             {
-                                totalEntropy += (int)lattice[(zIndex + ZUnit(vector) + 100) % 100, (yIndex + YUnit(vector) + 100) % 100, (xIndex + XUnit(vector) + 100) % 100] * (int)thisVacancy;
+                                // A-A makes a positive contribution
+                                // B-B makes a positive contribution
+                                // B-A and A-B make a negative contribution
+                                // A-0 and B-0 and 0-A and 0-B make zero contribution
+                                totalEntropy += (int)lattice[(zIndex + ZUnit(vector) + cellSize) % cellSize, (yIndex + YUnit(vector) + cellSize) % cellSize, (xIndex + XUnit(vector) + cellSize) % cellSize] * (int)thisVacancy;
                             }
                         }
                         xIndex++;
@@ -136,37 +323,81 @@ namespace Poly
                 zIndex++;
             }
 
-            var numChains = 1000;
-            var chainSize = 12;
-            var occupancies = new long[numChains * chainSize];
+            return totalEntropy;
+        }
 
-            bool stop = false;
-            var kT = 5000;
-            var acceptanceFactors = new decimal[12];
-            while (!stop)
+        private static Vector[] PopulateOccupancies(Random randomVacancySelector, int chainSize, int numChains)
+        {
+            // fill with some fake occupancies for now
+            // these are unphysical as they can be anywhere and are not adjacent
+
+            //var numChains = 1000;
+            //var chainSize = 12;
+            //return Enumerable.Repeat(0, numChains * chainSize - 1).Select(_ =>
+            //    new Vector((short)randomVacancySelector.Next(0, 100), (short)randomVacancySelector.Next(0, 100), (short)randomVacancySelector.Next(0, 100)))
+            //    .ToArray();
+
+            return new Vector[numChains * chainSize];
+        }
+
+
+        static void RenderLayerToConsole()
+        {
+            Console.Clear();
+
+            var builder = new StringBuilder();
+
+            short xIndex = 0;
+            short yIndex = 0;
+            short zIndex = 99;
+
+            //while (zIndex < cellSize)
+            //{
+            while (yIndex < cellSize)
             {
-                // precompute boltzmann factors
-                for (int i = 1; i < 12; i++)
+                while (xIndex < cellSize)
                 {
-                    acceptanceFactors[i - 1] = (decimal)Math.Exp((double)-i / kT);
+                    if ((xIndex + yIndex + zIndex) % 2 == 0)
+                    {
+                        char type = '#';
+                        switch (lattice[xIndex, yIndex, zIndex])
+                        {
+                            case VacancyType.A:
+                                type = 'A'; break;
+                            case VacancyType.B:
+                                type = 'B'; break;
+                            case VacancyType.Empty:
+                                type = '#'; break;
+                        }
+                        builder.Append(type);
+                        //Console.Write(type);
+                    }
+                    else
+                    {
+                        builder.Append(' ');
+                        //Console.Write(' ');
+                    }
+                    xIndex++;
+
                 }
+                xIndex = 0;
 
-                // perform annealing
-                var chosenVacancy = randomVacancySelector.Next(0, numChains * chainSize);
-                var chosenVacancyLocation = new int[]
-                {
-                    (int)(occupancies[chosenVacancy] >> 0) ^ 0b10000,
-                    (int)(occupancies[chosenVacancy] >> 16) ^ 0b10000,
-                    (int)(occupancies[chosenVacancy] >> 32) ^ 0b10000
-                };
+                yIndex++;
+                //Console.WriteLine();
+                builder.AppendLine();
 
-                kT /= 2;
-                stop = kT <= 1;
             }
 
-            Console.WriteLine($"Total entropy {totalEntropy}");
+            Console.Write(builder.ToString());
+            //    yIndex = 0;
+            //    zIndex++;
+            //Console.Read();
+            //}
+            //zIndex = 0;
 
-            Console.Read();
+            Console.WriteLine("Total Entropy: " + CountEntropy());
+
         }
+
     }
 }
