47aea6c11e5cc5d131173f807b541b3d99b289c0
Tom Wolverson <t.wolverson@reply.eu>
2020-07-06 20:39:33 +0100

    Rename tests

--- Poly.Tests/ShortestDistanceCalculations.cs
+++ Poly.Tests/ShortestDistanceCalculations.cs
@@ -4,40 +4,40 @@ using Xunit;
 
 namespace Poly.Tests
 {
-    public class DistanceCalculations
+    public class ShortestDistanceCalculations
     {
         private readonly Simulation simulation = new Simulation(100, 8, 0.5M);
 
         [Fact]
-        public void Test1()
+        public void From_99_To_1()
         {
             var distance = simulation.wrapDistance(99, 1);
             Assert.Equal(2, distance);
         }
 
         [Fact]
-        public void Test2()
+        public void From_99_To_2()
         {
             var distance = simulation.wrapDistance(99, 2);
             Assert.Equal(3, distance);
         }
 
         [Fact]
-        public void Test3()
+        public void From_98_To_99()
         {
             var distance = simulation.wrapDistance(98, 99);
             Assert.Equal(1, distance);
         }
 
         [Fact]
-        public void Test4()
+        public void From_1_To_99()
         {
             var distance = simulation.wrapDistance(1, 99);
             Assert.Equal(-2, distance);
         }
 
         [Fact]
-        public void Test5()
+        public void From_49_To_51()
         {
             var distance = simulation.wrapDistance(49, 51);
             Assert.Equal(2, distance);
@@ -45,7 +45,7 @@ namespace Poly.Tests
 
 
         [Fact]
-        public void Test6()
+        public void From_51_To_49()
         {
             var distance = simulation.wrapDistance(51, 49);
             Assert.Equal(-2, distance);
